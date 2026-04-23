2daeecd5cc0398507b9363965c3b1254bb6649a7
JuanMontezuma <juanjomontezuma@gmail.com>
2026-04-23 13:06:22 -0500

    incluir crud-funcional

--- gestion/views.py
+++ gestion/views.py
@@ -1,26 +1,37 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Autor, Libro
+from .forms import AutorForm, LibroForm
 
-# ============ CRUD GENÉRICO - LIBRO ============
+# ============ CRUD FUNCIONAL - AUTOR ============
 
-class LibroListView(ListView):
-    model = Libro
-    template_name = 'gestion/libro_list.html'
-    context_object_name = 'libros'
+def lista_autores(request):
+    autores = Autor.objects.all()
+    return render(request, 'gestion/lista_autores.html', {'autores': autores})
 
-class LibroCreateView(CreateView):
-    model = Libro
-    form_class = LibroForm
-    template_name = 'gestion/libro_form.html'
-    success_url = reverse_lazy('libro_list')
+def crear_autor(request):
+    if request.method == 'POST':
+        form = AutorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_autores')
+    else:
+        form = AutorForm()
+    return render(request, 'gestion/autor_form.html', {'form': form})
 
-class LibroUpdateView(UpdateView):
-    model = Libro
-    form_class = LibroForm
-    template_name = 'gestion/libro_form.html'
-    success_url = reverse_lazy('libro_list')
+def editar_autor(request, pk):
+    autor = get_object_or_404(Autor, pk=pk)
+    if request.method == 'POST':
+        form = AutorForm(request.POST, instance=autor)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_autores')
+    else:
+        form = AutorForm(instance=autor)
+    return render(request, 'gestion/autor_form.html', {'form': form})
 
-class LibroDeleteView(DeleteView):
-    model = Libro
-    template_name = 'gestion/libro_confirm_delete.html'
-    success_url = reverse_lazy('libro_list')
+def eliminar_autor(request, pk):
+    autor = get_object_or_404(Autor, pk=pk)
+    if request.method == 'POST':
+        autor.delete()
+        return redirect('lista_autores')
+    return render(request, 'gestion/autor_confirm_delete.html', {'autor': autor})
